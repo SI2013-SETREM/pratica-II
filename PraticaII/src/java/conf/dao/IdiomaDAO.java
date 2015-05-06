97815b243802baed6f16afa9557becf9a15bf92f
Dinei André Rockenbach <dineiar@gmail.com>
2015-05-05 21:56:43 -0300

    Código base do projeto da prática. Inclui todo o layout, o template padrão, tabela e formulário de Idiomas e páginas inicial e sobre.

---
+++ PraticaII/src/java/conf/dao/IdiomaDAO.java
@@ -0,0 +1,45 @@
+
+package conf.dao;
+
+import conf.model.Idiomas;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import util.HibernateUtil;
+
+public class IdiomaDAO {
+    private Session session;
+
+    public IdiomaDAO() {
+        session = HibernateUtil.getSessionFactory().openSession();
+    }
+    
+    public void insert(Idiomas i) {
+        Transaction t = session.beginTransaction();
+        session.save(i);
+        t.commit();
+    }
+    
+    public void update(Idiomas i) {
+        Transaction t = session.beginTransaction();
+        session.merge(i);
+        t.commit();
+    }
+    
+    public void delete(Idiomas i) {
+        Transaction t = session.beginTransaction();
+        session.delete(i);
+        t.commit();
+    }
+    
+    public Idiomas findById(int idi_codigo) {
+        return (Idiomas) session.load(Idiomas.class, idi_codigo);
+    }
+    
+    public List<Idiomas> findAll() {
+        Query q = session.createQuery("from Idiomas");
+        return q.list();
+    }
+    
+}
---
+++ PraticaII/web/resources/js/default.js
@@ -0,0 +1,104 @@
+$.extend($.fn.dataTable.defaults, {
+    "language": {
+        "emptyTable": "Nenhum registro encontrado",
+        "info": "Mostrando _START_ - _END_ de _TOTAL_ registros",
+        "infoEmpty": "Mostrando 0 - 0 de 0 registros",
+        "infoFiltered": "(Filtrados de _MAX_ registros)",
+        "infoPostFix": "",
+        "infoThousands": ".",
+        "lengthMenu": "_MENU_ registros por página",
+        "loadingRecords": "Carregando...",
+        "processing": "Processando...",
+        "zeroRecords": "Nenhum registro encontrado",
+        "search": "<i class='fa fa-search'></i>",
+        "paginate": {
+            "next": "Próxima",
+            "previous": "Anterior",
+            "first": "",
+            "last": ""
+        },
+        "aria": {
+            "sortAscending": ": Ordenar colunas de forma ascendente",
+            "sortDescending": ": Ordenar colunas de forma descendente"
+        }
+    },
+    "lengthMenu": [ 5, 10, 50, 100 ],
+    "renderer": "bootstrap",
+    "pagingType": "full_numbers",
+    "columnDefs": [ {
+        "targets": [-1, -2],
+        "orderable": false,
+        "searchable": false
+    } ],
+    "dom": '<"dataTables_wrapper form-inline dt-bootstrap no-footer"<"row"<"col-sm-6"i><"col-sm-6"f>><"row"<"col-sm-12"rt>><"row"<"col-sm-5"l><"col-sm-7"p>>>'
+});
+
+function toggleDropdown($a) {
+    var $parent = $a.parent();
+    if ($parent.hasClass('open')) {
+//        $a.children('.dropdown-chevron')
+//            .addClass('fa-chevron-left')
+//            .removeClass('fa-chevron-down')
+//                ;
+        $parent.children('.submenu')
+            .slideUp(400, function() {
+                $parent.removeClass('open');
+            });
+    } else {
+//        $a.children('.dropdown-chevron')
+//            .removeClass('fa-chevron-left')
+//            .addClass('fa-chevron-down')
+//                ;
+        $parent.children('.submenu')
+            .slideDown(400, function() {
+                $parent.addClass('open');
+            });
+    }
+}
+$(document).ready(function(){
+    
+    $('.menu-dropdown > a').click(function(e) {
+        $a = $(this);
+        $('.menu-dropdown.open > a').each(function() { //fecha os menus ativos
+            toggleDropdown($(this));
+            if ($(this).is($a)) {
+                $a = null;
+            }
+        });
+        if ($a)
+            toggleDropdown($a);
+    });
+    
+    $('.dataTable').DataTable();
+    
+    
+    // Ripple effect
+    var parent, ink, d, x, y;
+    $(".ripple a").click(function (e) {
+        parent = $(this).parent();
+        
+        //create .ink element if it doesn't exist
+        if (parent.find(".ink").length == 0)
+            parent.prepend("<span class='ink'></span>");
+
+        ink = parent.find(".ink");
+        //incase of quick double clicks stop the previous animation
+        ink.removeClass("animate");
+
+        //set size of .ink
+        if (!ink.height() && !ink.width())
+        {
+            //use parent's width or height whichever is larger for the diameter to make a circle which can cover the entire element.
+            d = Math.max(parent.outerWidth(), parent.outerHeight());
+            ink.css({height: d, width: d});
+        }
+
+        //get click coordinates
+        //logic = click coordinates relative to page - parent's position relative to page - half of self height/width to make it controllable from the center;
+        x = e.pageX - parent.offset().left - ink.width() / 2;
+        y = e.pageY - parent.offset().top - ink.height() / 2;
+
+        //set the position and add class .animate
+        ink.css({top: y + 'px', left: x + 'px'}).addClass("animate");
+    });
+});
