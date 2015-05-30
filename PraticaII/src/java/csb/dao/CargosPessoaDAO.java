f93ffe2c6a33985ecbbf7441e51be93dfe98a7b3
MateusHammes <cfelipeinformatica@gmail.com>
2015-05-29 21:41:26 -0300

    DAO de cargos pessoa, um metodo novo

--- PraticaII/src/java/csb/dao/CargosPessoaDAO.java
+++ PraticaII/src/java/csb/dao/CargosPessoaDAO.java
@@ -8,36 +8,52 @@ import org.hibernate.Transaction;
 import util.HibernateUtil;
 
 public class CargosPessoaDAO {
+
     private Session session;
 
     public CargosPessoaDAO() {
         session = HibernateUtil.getSessionFactory().openSession();
     }
-    
+
     public void insert(CargosPessoa c) {
         Transaction t = session.beginTransaction();
         session.save(c);
         t.commit();
     }
-    
+
     public void update(CargosPessoa c) {
         Transaction t = session.beginTransaction();
         session.merge(c);
         t.commit();
     }
-    
+
     public void delete(CargosPessoa c) {
         Transaction t = session.beginTransaction();
         session.delete(c);
         t.commit();
     }
-    
+
     public CargosPessoa findById(int pes_codigo) {
         return (CargosPessoa) session.load(CargosPessoa.class, pes_codigo);
     }
-    
+
     public List<CargosPessoa> findAll() {
         Query q = session.createQuery("from CargosPessoa");
         return q.list();
     }
+
+    ////-Metodos novos
+    public List<CargosPessoa> GetListCargoPessoa(int pes_codigo, int car_codigo) {///utilizado para selecionar todas as pessoas de determinado cargo ou todos os cargos de determinada pessao
+        String sql = "";
+        if (pes_codigo != 0) {
+            sql += " and pes_codigo = " + pes_codigo;
+        }
+        if (car_codigo != 0) {
+            sql += " and car_codigo = " + car_codigo;
+        }
+
+        Query q = session.createQuery("from CargosPessoa where 1=1" + sql);
+        return q.list();
+    }
+
 }
