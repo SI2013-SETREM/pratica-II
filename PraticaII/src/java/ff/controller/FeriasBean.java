3d46aae1dce5a526b4c2223ccc3a677999b4582d
MaxNaegele <maxnaegele2010@gmail.com>
2015-06-14 22:48:25 -0300

    bean de ff

---
+++ PraticaII/src/java/ff/controller/FeriasBean.java
@@ -0,0 +1,91 @@
+package ff.controller;
+
+import cfg.dao.PessoaDAO;
+import cfg.model.Pessoa;
+import ff.dao.FaltaDAO;
+import ff.dao.FeriasDAO;
+import ff.model.Falta;
+import ff.model.Ferias;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import javax.servlet.http.HttpServletRequest;
+
+@ManagedBean
+public class FeriasBean {
+
+   
+    private Pessoa pessoa = new Pessoa();
+    private PessoaDAO pessoaDAO = new PessoaDAO();
+    private DataModel<Pessoa> pessoas;
+    private int idPessoa;
+    
+    private final FeriasDAO feriasDAO = new FeriasDAO();
+    private Ferias ferias = new Ferias();
+
+    public FeriasBean() {
+    }
+
+    public String inicio() {
+
+        pessoas =  (DataModel<Pessoa>) pessoaDAO.findByPessoaId(getIdPessoa());
+        pessoa = pessoaDAO.findById(pessoa.getPes_codigo());
+        return "faltafrm";
+    }
+
+    public String insert() {
+        feriasDAO.insert(ferias);
+        return "fichafunfrm";
+    }
+    public String cancelar() {  
+        return "fichafunfrm";
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public PessoaDAO getPessoaDAO() {
+        return pessoaDAO;
+    }
+
+    public void setPessoaDAO(PessoaDAO pessoaDAO) {
+        this.pessoaDAO = pessoaDAO;
+    }
+
+    public DataModel getPessoas() {
+        return pessoas;
+    }
+
+    public void setPessoas(DataModel pessoas) {
+        this.pessoas = pessoas;
+    }
+
+
+    public Ferias getFerias() {
+        return ferias;
+    }
+
+    public void setFerias(Ferias ferias) {
+        this.ferias = ferias;
+    }
+
+    public int getIdPessoa() {
+        return idPessoa;
+    }
+
+    public void setIdPessoa(int idPessoa) {
+        this.idPessoa = idPessoa;
+    }
+
+ 
+}
